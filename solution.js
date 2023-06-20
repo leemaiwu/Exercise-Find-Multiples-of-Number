e9f6dd2d4a16b42c9cc9fa33c8bd76f828c0cdc4
Laura Wu <leemaiwu@gmail.com>
2023-06-20 15:38:16 -0600

    Completed with while and if loop

--- solution.js
+++ solution.js
@@ -1,1 +1,13 @@
-const
+const findMultiples = (integer, limit) => {
+   let multiples = []
+   let startingNum = integer
+   while (startingNum <= limit) {
+    if (startingNum % integer === 0) {
+        multiples.push(startingNum)
+    }
+    startingNum++
+   }
+   return multiples
+}
+
+console.log(findMultiples(2, 6)) // [2, 4, 6]
